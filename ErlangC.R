f594bb8cda328f70a629412b724d09017f78f088
sbastable <stephenbastable@googlemail.com>
2016-08-14 22:10:25 +0000

    major revision and deploy
    first draft of ErlangC lib

--- ErlangC.R
+++ ErlangC.R
@@ -6,21 +6,65 @@
 #' @return returns the probability as a decimal between 0 and 1 of an individual call waiting
 #' @export
 #'
-#' @examples ErlangC_Base(20,(500*(600/3600)))
+#' @examples ErlangC_Base(20,(500*(600/3600))) where 0 is the number of agents, 500 is the AHT in seconds, 600 is the number of calls forecast in one hour and 3600 is the number of seconds in one hour.
 ErlangC_Base <- function(m,u)
 {
   # Calculates base Erlang C probability of waiting
-}
-  
-ErlangC_GetN <- function(x)
-{
-  # Steve Bastable 2016-08-13
-  # Solves ErlangC service level equation for N (number of agents)
-  # http://www.mitan.co.uk/erlang/elgcmath.htm gives an excellent overview of this
-  b = x^2
-  return(b)
+  p <- u/m # set var p to be u/m, which is known as occupancy or utilisation in common call centre terminology
+  # sum of u^k / fact(k) over set 0 < k < m - iterative over sum
+  xtoy_overyfact <- function(x,y) {return((x ** y)/factorial(y))}
+  ssum <- sum(xtoy_overyfact(u,0:(m-1)))
+  numerator <- xtoy_overyfact(u,m)
+  denominator <- (numerator) + ((1-p)*ssum)
+  return (numerator/denominator)
 }
 
-print(Square(4))
-print(Square(x=4)) # same thing
+
+print(ErlangC_Base(55,48))
+
+ErlangC_ASA <- function(m,u,t) # m=number of agents, u=traffic intensity, t=AHT time in seconds
+{
+  p <- u/m # set var p to be u/m, which is known as occupancy or utilisation in common call centre terminology
+  numerator <- ErlangC_Base(m,u) * t
+  denominator <- m * (1-p)
+  return(numerator/denominator)
+}
+
+print(ErlangC_ASA(55,48,240))
+
+
+ErlangC_ServiceLevel <- function(m,u,t,st) # m=number of agents, u=traffic intensity, t=AHT time in seconds, st=desired answer within threshold (secs)
+{
+ exponent <- -(m-u) * (st/t)
+ invsl <- (ErlangC_Base(55,48) * exp(exponent)) # exp() = e^()
+ return(1-invsl)
+}
+print(ErlangC_ServiceLevel(55,48,240,15))
+
+
+
+ErlangC_GetRequiredAgents <- function(u,st,t,sl) # u is traffic intensity, st is desired service time in seconds, t=AHT time in seconds, sl is desired service level as decimal
+{
+  continue <- TRUE
+  m <- floor(u) + 1 #initialise m (number of agents) set at integer intensity+1
+  
+  while(continue)
+  {
+    
+    
+    expectedsl <- ErlangC_ServiceLevel(m,u,t,st)
+    
+  
+    if(expectedsl >= sl)
+    {
+      continue <- FALSE
+    }
+    
+    m <- m+1
+  }
+  return(m)
+}
+print(ErlangC_GetRequiredAgents(48,15,240,.845))
+
+
 
